c3111e23442158cbb4d042c42581b26569b26294
Jorge Hidalgo <jorgerhid@hotmail.com>
2022-09-03 19:29:28 -0300

    Colores

--- index.js
+++ index.js
@@ -1,106 +1,113 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(cors())
-app.use(express.json())
+app.use(express.static("public"));
+app.use(cors());
+app.use(express.json());
 
-const jugadores = []
+const jugadores = [];
 
 class Jugador {
   constructor(id) {
-    this.id = id
+    this.id = id;
   }
 
   asignarMokepon(mokepon) {
-    this.mokepon = mokepon
+    this.mokepon = mokepon;
   }
 
   actualizarPosicion(x, y) {
-    this.x = x
-    this.y = y
+    this.x = x;
+    this.y = y;
   }
 
   asignarAtaques(ataques) {
-    this.ataques = ataques
+    this.ataques = ataques;
   }
 }
 
 class Mokepon {
   constructor(nombre) {
-    this.nombre = nombre
+    this.nombre = nombre;
   }
 }
 
 app.get("/unirse", (req, res) => {
-  const id = `${Math.random()}`
+  const id = `${Math.random()}`;
 
-  const jugador = new Jugador(id)
+  const jugador = new Jugador(id);
 
-  jugadores.push(jugador)
+  jugadores.push(jugador);
 
-  res.setHeader("Access-Control-Allow-Origin", "*")
-  
-  res.send(id)
-})
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.send(id);
+});
 
 app.post("/mokepon/:jugadorId", (req, res) => {
-  const jugadorId = req.params.jugadorId || ""
-  const nombre = req.body.mokepon || ""
-  const mokepon = new Mokepon(nombre)
-  
-  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+  const jugadorId = req.params.jugadorId || "";
+  const nombre = req.body.mokepon || "";
+  const mokepon = new Mokepon(nombre);
+
+  const jugadorIndex = jugadores.findIndex(
+    (jugador) => jugadorId === jugador.id
+  );
 
   if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].asignarMokepon(mokepon)
+    jugadores[jugadorIndex].asignarMokepon(mokepon);
   }
-  
-  console.log(jugadores)
-  console.log(jugadorId)
-  res.end()
-})
+
+  console.log(jugadores);
+  console.log(jugadorId);
+  res.end();
+});
 
 app.post("/mokepon/:jugadorId/posicion", (req, res) => {
-  const jugadorId = req.params.jugadorId || ""
-  const x = req.body.x || 0
-  const y = req.body.y || 0
+  const jugadorId = req.params.jugadorId || "";
+  const x = req.body.x || 0;
+  const y = req.body.y || 0;
 
-  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+  const jugadorIndex = jugadores.findIndex(
+    (jugador) => jugadorId === jugador.id
+  );
 
   if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].actualizarPosicion(x, y)
+    jugadores[jugadorIndex].actualizarPosicion(x, y);
   }
 
-  const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
+  const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id);
 
   res.send({
-    enemigos
-  })
-})
+    enemigos,
+  });
+});
 
 app.post("/mokepon/:jugadorId/ataques", (req, res) => {
-  const jugadorId = req.params.jugadorId || ""
-  const ataques = req.body.ataques || []
-  
-  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+  const jugadorId = req.params.jugadorId || "";
+  const ataques = req.body.ataques || [];
+
+  const jugadorIndex = jugadores.findIndex(
+    (jugador) => jugadorId === jugador.id
+  );
 
   if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].asignarAtaques(ataques)
+    jugadores[jugadorIndex].asignarAtaques(ataques);
   }
 
-  res.end()
-})
+  res.end();
+});
 
 app.get("/mokepon/:jugadorId/ataques", (req, res) => {
-  const jugadorId = req.params.jugadorId || ""
-  const jugador = jugadores.find((jugador) => jugador.id === jugadorId)
+  const jugadorId = req.params.jugadorId || "";
+  const jugador = jugadores.find((jugador) => jugador.id === jugadorId);
   res.send({
-    ataques: jugador.ataques || []
-  })
-})
+    ataques: jugador.ataques || [],
+  });
+});
 
 app.listen(8080, () => {
-  console.log("Servidor funcionando")
-})
+  console.log("Servidor funcionando");
+});
+"#8940ff"
